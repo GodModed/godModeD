34f96e2d560570dcbeb1a184b7bcfb3760f7823c
godmodeD <djlevy26@gmail.com>
2024-02-08 18:15:45 -0500

    better looking

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+
+const THEME = 'graywhite';
+
+const externalSvgs = [`https://github-readme-stats.vercel.app/api?username=GodModed&show_icons=true&theme=${THEME}&hide_border=true`,
+    `https://github-readme-stats.vercel.app/api/top-langs/?username=Godmoded&layout=compact&theme=${THEME}&hide_border=true`,
+    `https://github-readme-stats.vercel.app/api/wakatime?username=GodMode&theme=${THEME}&layout=compact&langs_count=12&hide_border=true`]
 
 // december 29, 2007
 const birthday = new Date('2007-12-29');
@@ -9,16 +16,30 @@ let count = parseInt(fs.readFileSync('count.txt', 'utf8')) || 0;
 
 const svg = fs.readFileSync('readme.svg', 'utf8');
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     count++;
     let replacedSvg = svg
         .replace('{viw}', count)
         .replace('{age}', Math.floor((10 * (new Date() - birthday) / 31556952000)) / 10);
+    
+    let stats = await getReadmeStats();
+    replacedSvg = replacedSvg.replace('{stats}', stats.join('\n'));
+    
 
     res.set('Content-Type', 'image/svg+xml');
     res.set('Cache-Control', 'public, max-age=0, must-revalidate');
     res.send(replacedSvg);
 });
+
+async function getReadmeStats() {
+    let promises = externalSvgs.map(url => axios.get(url));
+    let results = await Promise.all(promises);
+    results = results.map((result, i) => {
+        return "<g transform='translate(100, " + (i * 200 + 250) + ")'>" + result.data + "</g>";
+    });
+
+    return results;
+}
 
 
 app.listen(9008, () => {
