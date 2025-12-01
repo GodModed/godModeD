a36d886ca2312190ac76ddcb8a1fed53dbdee136
Dylan <djlevy26@gmail.com>
2025-11-30 22:45:22 -0500

    add cache

--- stats.ts
+++ stats.ts
@@ -7,7 +7,32 @@ const THEME = 'graywhite';
 const WAKATIME_USERNAME = 'GodMode';
 const GITHUB_USERNAME = 'Godmoded';
 
+export const CACHE: {
+    data: {
+        wakaTime: string | null;
+        githubStats: string | null;
+    };
+    timestamp: number;
+    duration: number;
+} = {
+    data: {
+        wakaTime: null,
+        githubStats: null,
+    },
+    timestamp: 0,
+    duration: 24 * 60 * 60 * 1000, // 1 day
+};
+
 export default async function getStats(): Promise<string> {
+    const now = Date.now();
+    if (
+        CACHE.data.wakaTime &&
+        CACHE.data.githubStats &&
+        now - CACHE.timestamp < CACHE.duration
+    ) {
+        return [CACHE.data.wakaTime, CACHE.data.githubStats].join('\n');
+    }
+
     const stats = await fetchWakatimeStats({
         username: WAKATIME_USERNAME,
         api_domain: 'wakatime.com',
@@ -29,6 +54,10 @@ export default async function getStats(): Promise<string> {
         /* @ts-ignore */
         theme: THEME
     }).replace("<svg", `<svg x="100px" y="450px"`);
+
+    CACHE.data.wakaTime = wakaTimeCard;
+    CACHE.data.githubStats = githubStatsCard;
+    CACHE.timestamp = now;
 
     return [wakaTimeCard, githubStatsCard].join('\n');
 }
